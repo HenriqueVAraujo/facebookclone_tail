4c5fdb4934af21bb31d42ca2223498aba42f4eac
HenriqueVAraujo <henriquevasconcelosaraujo@gmail.com>
2022-05-25 00:28:34 -0300

    Primeiro Clone do facebook

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      spacing: {
+        '30': '7.5rem',
+        '290px': '290px',
+        '396px': '396px',
+        '500px': '500px',
+        '980px': '980px',
+      },
+      fontSize: {
+        'subititle': '1.7rem',
+      },
+      colors: {
+        'main-gray': '#F0F2F5',
+        'main-blue': '#1877f2',
+        'main-blue-hover': '#166fe5',
+        'main-green': '#42b72a',
+        'main-green-hover': '#36a420',
+      },
+    },
+  },
+  plugins: [],
+}
